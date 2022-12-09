ca29f6b242425164660fbbb3136ea32b77ec9b36
Jose Guilherme <jgnvs6@gmail.com>
2022-12-08 22:25:35 -0300

    test: should return 400 if validation fails

--- src/interface/controllers/sing-up.controller.ts
+++ src/interface/controllers/sing-up.controller.ts
@@ -23,7 +23,7 @@ export class SingUpController implements ISingUpController {
     const hasError = this.validation.validate(request);
 
     if (hasError) {
-      badRequest(hasError);
+      return badRequest(hasError);
     }
 
     try {
