66e142d7e7614ae1274ddad9e1516bd5dceeb5b3
Jose Guilherme <jgnvs6@gmail.com>
2022-12-08 22:46:52 -0300

    test: should return 400 if usecase throws known error

--- tests/common/helpers/test.helper.ts
+++ tests/common/helpers/test.helper.ts
@@ -1,3 +1,12 @@
+import { DefaultException, ExceptionType } from '@/common/helpers';
+
 export const throwError = (): never => {
   throw new Error();
+};
+
+export const throwKnownError = (): never => {
+  throw new DefaultException({
+    code: 'KNOWN_ERROR',
+    type: ExceptionType.USER
+  });
 };
--- tests/interface/controllers/sign-up.controller.spec.ts
+++ tests/interface/controllers/sign-up.controller.spec.ts
@@ -5,7 +5,7 @@ import { Validation } from '@/interface/protocols';
 
 import { dataFakerUser } from '@/tests/domain/user.data.faker';
 import { SingUpUseCase } from '@/application/usecases';
-import { throwError } from '@/tests/common/helpers';
+import { throwError, throwKnownError } from '@/tests/common/helpers';
 
 const makeSut = () => {
   const validation = createMock<Validation>();
@@ -114,6 +114,26 @@ describe('SignUp Controller', () => {
     expect(singUpUseCaseSpy).toBeCalledTimes(1);
     expect(singUpUseCaseSpy).toBeCalledWith(params);
     expect(result.statusCode).toBe(500);
+  });
+
+  test('Should return 400 if SingUpUseCase throws known error', async () => {
+    const { sut, validateSpy, singUpUseCaseSpy } = makeSut();
+
+    const { id, ...params } = dataFakerUser();
+
+    singUpUseCaseSpy.mockImplementationOnce(throwKnownError);
+
+    const result = await sut.handle({
+      email: params.email,
+      name: params.name,
+      password: params.password
+    });
+
+    expect(validateSpy).toBeCalledTimes(1);
+    expect(validateSpy).toBeCalledWith(params);
+    expect(singUpUseCaseSpy).toBeCalledTimes(1);
+    expect(singUpUseCaseSpy).toBeCalledWith(params);
+    expect(result.statusCode).toBe(400);
   });
 
   test('Should return 201 on sucess', async () => {
