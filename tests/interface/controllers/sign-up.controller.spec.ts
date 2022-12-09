9c384210757d369979d91fcb6873439554b017a9
Jose Guilherme <jgnvs6@gmail.com>
2022-12-08 21:51:13 -0300

    test: should call SingUpUseCase with correct values

--- tests/interface/controllers/sign-up.controller.spec.ts
+++ tests/interface/controllers/sign-up.controller.spec.ts
@@ -4,17 +4,21 @@ import { SingUpController } from '@/interface/controllers';
 import { Validation } from '@/interface/protocols';
 
 import { dataFakerUser } from '@/tests/domain/user.data.faker';
+import { SingUpUseCase } from '@/application/usecases';
 
 const makeSut = () => {
   const validation = createMock<Validation>();
   const validateSpy = jest.spyOn(validation, 'validate');
-  ;
 
-  const sut = new SingUpController(validation);
+  const singUpUseCase = createMock<SingUpUseCase>();
+  const singUpUseCaseSpy = jest.spyOn(singUpUseCase, 'execute');
+
+  const sut = new SingUpController(validation, singUpUseCase);
 
   return {
     sut,
-    validateSpy
+    validateSpy,
+    singUpUseCaseSpy
   };
 };
 
@@ -36,5 +40,22 @@ describe('SignUp Controller', () => {
 
     expect(validateSpy).toBeCalledTimes(1);
     expect(validateSpy).toBeCalledWith(params);
+  });
+
+  test('Should call SingUpUseCase with correct values', async () => {
+    const { sut, validateSpy, singUpUseCaseSpy } = makeSut();
+
+    const { id, ...params } = dataFakerUser();
+
+    await sut.handle({
+      email: params.email,
+      name: params.name,
+      password: params.password
+    });
+
+    expect(validateSpy).toBeCalledTimes(1);
+    expect(validateSpy).toBeCalledWith(params);
+    expect(singUpUseCaseSpy).toBeCalledTimes(1);
+    expect(singUpUseCaseSpy).toBeCalledWith(params);
   });
 });
