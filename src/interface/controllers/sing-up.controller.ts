605687e0d58c7efd642d7d155d6b044e8487d20a
Jose Guilherme <jgnvs6@gmail.com>
2022-12-08 21:51:05 -0300

    feat: should call SingUpUseCase with correct values

--- src/interface/controllers/sing-up.controller.ts
+++ src/interface/controllers/sing-up.controller.ts
@@ -1,6 +1,8 @@
 
 import { Controller, HttpResponse, Validation } from '@/interface/protocols';
 
+import { SingUpUseCase } from '@/application/usecases';
+
 export interface ISingUpController extends Controller<ISingUpController.Params> {}
 
 export namespace ISingUpController {
@@ -14,10 +16,16 @@ export namespace ISingUpController {
 }
 
 export class SingUpController implements ISingUpController {
-  constructor (private readonly validation: Validation) {}
+  constructor (private readonly validation: Validation, private readonly singUpUseCase: SingUpUseCase) {}
   async handle (request: ISingUpController.Params): Promise<HttpResponse> {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const hasError = this.validation.validate(request);
+
+    try {
+      await this.singUpUseCase.execute(request);
+    } catch (error) {
+
+    }
 
     return null;
   }
